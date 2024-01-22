e61fe6565ff78cbae52193612f38364bcc1425fb
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2024-01-22 13:14:42 +0900

    add: beginner/1st

--- src/app/beginner/1st/page.tsx
+++ src/app/beginner/1st/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { Canvas } from '@react-three/fiber';
+
 export default function Page1st() {
-  return <div>page 1st</div>;
+  return (
+    <div id='canvas-container'>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color='blue' position={[0, 0, 5]} />
+        <mesh>
+          <sphereGeometry args={[2, 2, 2]} />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
